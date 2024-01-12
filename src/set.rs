ba581b403559c0320e0d379ab9466643122b0b89
Xiaoyue Chen <xchen@vvvu.org>
2024-01-12 06:43:58 +0100

    Update license

--- src/set.rs
+++ src/set.rs
@@ -1,17 +1,20 @@
+// mcm --- a Multiple Constant Multiplication solver
 // Copyright (C) 2024  Xiaoyue Chen
 //
-// This program is free software: you can redistribute it and/or modify
+// This file is part of mcm.
+//
+// mcm is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
+// mcm is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// along with mcm.  If not, see <http://www.gnu.org/licenses/>.
 
 use crate::primitive::Natural;
 use crate::primitive::Odd;
